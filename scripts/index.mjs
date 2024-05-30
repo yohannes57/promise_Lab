3683f158d2a0066a53792c7129e0d5bba972fdbe
yohannes57 <ytmderes@gmail.com>
2024-05-30 11:40:13 -0700

    second way added

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,6 +1,9 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
+
 import { central, db1, db2, db3, vault } from "./databases.mjs";
-console.log("am working ...");
+
+let startTime=performance.now()
+// const endTime=performance.now();
 const getUserData = async (id) => {
   const dbs = {
     db1: db1,
@@ -8,12 +11,17 @@ const getUserData = async (id) => {
     db3: db3,
   };
   let Which_db = await central(id);
-  let returnedValue;
+  
+  let returnedValue={};
+
+  
   try {
+    returnedValue["id"]=id;
     switch (Which_db) {
       case "db1":
         await db1(id).then((data) => {
           dbs.db1 = data;
+          returnedValue["db1"]=data;
         });
         // dbs.db1 = returnedValue;
         console.log("user data-1 ..", dbs.db1);
@@ -21,6 +29,7 @@ const getUserData = async (id) => {
       case "db2":
         await db2(id).then((data) => {
           dbs.db2 = data;
+          returnedValue["db2"]=data;
           console.log("user data -2 ", dbs.db2);
         });
         // dbs.db2 = returnedValue;
@@ -29,6 +38,7 @@ const getUserData = async (id) => {
       case "db3":
         await db3(id).then((data) => {
           dbs.db3 = data;
+          returnedValue["db3"]=data;
           console.log("usedata -3 ", dbs.db3);
         });
         break;
@@ -37,44 +47,39 @@ const getUserData = async (id) => {
       default:
         throw new error("unknow database");
     }
-    if (id >= 1 && id <= 10) {
+
       const vaultData = await vault(id);
       console.log(vaultData);
-      return { id, dbs, vaultData };
-    } else {
-      return { id, dbs };
-    }
+
+      returnedValue=Object.assign(returnedValue,returnedValue,vaultData)
+      return returnedValue;
   } catch (err) {
-    console.log(err);
+    console.error(err);
   }
 };
-getUserData(6);
-// const returnedValue = await db1(4)
-// .then((e)=>{
-//   console.log(e.username,e.website,e.company)
-// })
+getUserData(5);
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////second way
+//or it can be done 
 /*
-{
-    id: number,
-    name: string,
-    username: string,
-    email: string,
-    address: {
-      street: string,
-      suite: string,
-      city: string,
-      zipcode: string,
-      geo: {
-        lat: string,
-        lng: string
-      }
-    },
-    phone: string,
-    website: string,
-    company: {
-      name: string,
-      catchPhrase: string,
-      bs: string
-    }
+let profile={};
+try {
+   let getData=await dbs[Which_db](id);
+         profile["id"]=id;
+          const vaultData = await vault(id);
+          console.log(vaultData);
+        
+profile=Object.assign(profile,getData,vaultData);
+console.log(profile)
+return profile;
+} catch (error) {
+  console.error(error)
+  
 }
+}
+const endTime=performance.now();
+getUserData(5);
+console.log((endTime-startTime)*1000);
 */
